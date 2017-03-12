378382823991b409d0916ecd6eba561ec5332252
UTeste <user.teste.atividade@gmail.com>
2017-03-12 20:40:54 -0300

    Adição do teste referente a subtração

--- teste.py
+++ teste.py
@@ -1,6 +1,10 @@
 import pytest
 from main import somar
 
-def teste_da_soma():
+def teste_soma():
         """docstring for test somar"""
         assert somar(10,3) == 13
+
+def teste_subtrair():
+	"""docstring for test subtrair"""
+	assert subtrair(11,7) == 4
